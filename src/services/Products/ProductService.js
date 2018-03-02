6af2e4815e862fad62a2c35faac73fa7b31e7364
a mac <amac250@as-Mac-mini.local>
2018-03-01 18:29:50 -0800

    functional bits

--- src/services/Products/ProductService.js
+++ src/services/Products/ProductService.js
@@ -1,21 +1,20 @@
 const ProductService =  {
 
     listProductsInCart: (cart, products) => {
-     const results = Object.keys(cart.lst).reduce( (acc, el) => {
-       const res = products
-       .filter(elem => (cart.lst[el] &&  elem.id === parseInt(el, 10)))
-       .map(({id, name, img, descr}) => {
-         return { id, name, img, descr: `${descr[0]} ...`, qty: cart.lst[el] } 
-       });
-       return [...acc, ...res];
-    },[]);
-    return results;
+     return Object.keys(cart.lst).reduce( (acc, el) => {
+        const res = products
+        .filter(elem => (cart.lst[el] &&  elem.id === parseInt(el, 10)))
+        .map(({id, name, img, descr}) => {
+          return { id, name, img, descr: `${descr[0]} ...`, qty: cart.lst[el] } 
+        });
+        return [...acc, ...res];
+      },[]);
     },
 
     sumCartQty: (cart) => {
       return Object.keys(cart.lst).reduce((acc,el) => {return acc+cart.lst[el] }, 0);
     },
-    
+
     loadProductKeywords: () => {
       return [
         {full:"french", short:'fr'},
